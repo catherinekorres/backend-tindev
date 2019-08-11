8eeb952aa8d6f2e04d98660ff61b6138b97881c3
korrescatherine <cathy.korres@gmail.com>
2019-08-11 14:00:58 -0300

    connection to mongodb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://catherinekorres:gPmfmkuZa3WdlYJC@cluster0-aygqp.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
